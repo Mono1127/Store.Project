dfb49f32b61cd7b2a4f8446d5666305143f8cbe9
Mono1127 <mohandwael27@gmail.com>
2025-10-23 23:52:06 +0300

    sec 3

---
+++ Core/Store.Project.Services.Abstractions/Products/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Store.Project.Shared.Dtos.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Project.Services.Abstractions.Products
+{
+    public interface IProductService
+    {
+
+        Task <IEnumerable<ProductResponse>> GetAllProductsAsync ();
+    }
+}
