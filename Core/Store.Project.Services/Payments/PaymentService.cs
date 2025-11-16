759d367aba09bdf2e56d8029f65049ce35ac80ff
Mono1127 <mohandwael27@gmail.com>
2025-11-16 20:12:10 +0200

    End Of Project

--- Core/Store.Project.Services/Payments/PaymentService.cs
+++ Core/Store.Project.Services/Payments/PaymentService.cs
@@ -44,7 +44,7 @@ namespace Store.Project.Services.Payments
             var amount = subTotal + deliveryMethod.Price;
 
 
-            StripeConfiguration.ApiKey = _configuration["StripeOptions:secretKey0"];
+            //StripeConfiguration.ApiKey = _configuration["StripeOptions:secretKey0"];
 
             PaymentIntentService paymentIntentService = new PaymentIntentService();
             PaymentIntent paymentIntent;
